7b1e265525487c849feb02425361e4cc93070b4d
Daniel Reichelt <dr1400@web.de>
2014-12-22 11:29:59 +0100

    Added Tooltip widget

---
+++ WidgetUI/Utils/FollowMouse.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace WidgetUI
+{
+	[DisallowMultipleComponent]
+	[RequireComponent(typeof(RectTransform))]
+	[AddComponentMenu("WidgetUI/Follow Mouse")]
+	public class FollowMouse 
+		: UIBehaviour
+	{
+		[SerializeField]
+		private TextAnchor m_anchor = TextAnchor.UpperLeft;
+
+		[SerializeField]
+		private Vector2 m_offset = Vector2.zero;
+
+		[SerializeField]
+		[Tooltip("If enabled, this object will stick to the mouse. Otherwise, the position will be set once on initialization.")]
+		private bool m_follow = true;
+
+		[SerializeField]
+		[Tooltip("If enabled, this object will be kept inside the parent canvas")]
+		private bool m_preventOverflow = true;
+
+		#region Properties
+		public Vector2 Offset
+		{
+			get
+			{
+				return m_offset;
+			}
+			set
+			{
+				m_offset = value;
+			}
+		}
+
+		public TextAnchor Anchor
+		{
+			get
+			{
+				return m_anchor;
+			}
+			set
+			{
+				m_anchor = value;
+				this.RectTransform.pivot = this.CalculatePivotFromAnchor(m_anchor);
+			}
+		}
+
+		public bool Follow
+		{
+			get
+			{
+				return m_follow;
+			}
+			set
+			{
+				m_follow = value;
+			}
+		}
+
+		// RectTransform reference and caching mechanism
+		[NonSerialized]
+		private RectTransform m_rectTransform;
+		RectTransform RectTransform
+		{
+			get
+			{
+				if (m_rectTransform == null)
+				{
+					m_rectTransform = this.GetComponent<RectTransform>();
+				}
+				return m_rectTransform;
+			}
+		}
+
+		// Canvas transform reference and caching mechanism
+		[NonSerialized]
+		private RectTransform m_canvasTransform;
+		RectTransform Canvas
+		{
+			get
+			{
+				if (m_canvasTransform == null)
+				{
+					Canvas canvas = this.GetComponentInParent<Canvas>();
+					if(canvas != null)
+					{
+						m_canvasTransform = canvas.GetComponent<RectTransform>();
+					}
+				}
+				return m_canvasTransform;
+			}
+		}
+		#endregion
+
+		protected override void Start()
+		{
+			base.Start();
+
+			this.RectTransform.pivot = this.CalculatePivotFromAnchor(m_anchor);
+			this.RectTransform.anchorMin = Vector2.zero;
+			this.RectTransform.anchorMax = Vector2.zero;
+
+			this.UpdatePosition();
+		}
+
+		/// <summary>
+		/// Update the position if the RectTransform changes
+		/// </summary>
+		protected override void OnRectTransformDimensionsChange()
+		{
+			base.OnRectTransformDimensionsChange();
+			this.UpdatePosition();
+		}
+
+		private void LateUpdate()
+		{
+			if(m_follow)
+			{
+				this.UpdatePosition();
+			}
+		}
+
+		private void UpdatePosition()
+		{
+			Vector3 mousePos = Input.mousePosition;
+			Vector2 mouseScreenPos = new Vector2(mousePos.x, mousePos.y);
+			Vector2 anchoredPosition = mouseScreenPos + m_offset;
+
+			this.RectTransform.anchoredPosition = anchoredPosition;
+
+			if(m_preventOverflow)
+			{
+				Rect canvasRect = this.Canvas.GetWorldRect();
+				Rect ownRect = this.RectTransform.GetWorldRect();
+
+				Vector2 offset = Vector2.zero;
+
+				float dx1 = ownRect.xMin - canvasRect.xMin;
+				float dx2 = ownRect.xMax - canvasRect.xMax;
+				float dy1 = ownRect.yMin - canvasRect.yMin;
+				float dy2 = ownRect.yMax - canvasRect.yMax;
+
+				if (dx1 < 0)
+				{
+					offset.x -= dx1;
+				}
+				else if(dx2 > 0)
+				{
+					offset.x -= dx2;
+				}
+
+				if (dy1 < 0)
+				{
+					offset.y -= dy1;
+				}
+				else if (dy2 > 0)
+				{
+					offset.y -= dy2;
+				}
+
+				if (offset != Vector2.zero)
+				{
+					anchoredPosition += offset;
+					this.RectTransform.anchoredPosition = anchoredPosition;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Calculates the normalized pivot position from a TextAnchor enumeration
+		/// </summary>
+		/// <param name="p_anchor"></param>
+		/// <returns></returns>
+		private Vector2 CalculatePivotFromAnchor(TextAnchor p_anchor)
+		{
+			float x = ((int)p_anchor % 3) * .5F;
+			float y = 1.0F - ((int)p_anchor / 3) * .5F;
+			return new Vector2(x, y);
+		}
+	}
+}
---
+++ WidgetUI/Widgets/Tooltip/Tooltip.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace WidgetUI
+{
+	public abstract class Tooltip<T, WidgetType> 
+		: UIBehaviour
+		, IPointerEnterHandler
+		, IPointerExitHandler
+		where WidgetType : UIBehaviour, IWidget<T>
+	{
+		protected abstract T GetContent();
+		protected abstract IWidgetAllocator<WidgetType> GetWidgetAllocator();
+
+		protected WidgetType m_widget;
+		protected IWidgetAllocator<WidgetType> m_allocator;
+
+		[SerializeField]
+		protected float m_delay = 0.8F;
+
+		public float Delay
+		{
+			get
+			{
+				return m_delay;
+			}
+			set
+			{
+				m_delay = value;
+			}
+		}
+
+
+		protected virtual void Construct()
+		{
+			m_widget = null;
+			m_allocator = this.GetWidgetAllocator();
+		}
+
+		protected virtual void Destruct()
+		{
+			if (m_widget != null)
+			{
+				this.Hide();
+			}
+		}
+
+		protected override void Awake()
+		{
+			base.Awake();
+			this.Construct();
+		}
+
+		protected override void OnDestroy()
+		{
+			base.OnDestroy();
+			this.Destruct();
+		}
+
+		protected virtual void Show()
+		{
+			if(m_widget != null)
+			{
+				this.Hide();
+			}
+
+			m_widget = this.CreateTooltipWidget();
+			m_widget.transform.SetParent(this.GetTooltipParent(), false);
+
+			this.AddDefaultComponents(m_widget);
+		}
+
+		protected virtual void Hide()
+		{
+			if (m_widget != null)
+			{
+				m_widget.Disable();
+				m_allocator.Destroy(m_widget);
+				m_widget = null;
+			}
+		}
+
+		protected virtual WidgetType CreateTooltipWidget()
+		{
+			T content = this.GetContent();
+			WidgetType widget = m_allocator.Construct();
+			widget.Enable(content);
+
+			return widget;
+		}
+
+		protected virtual void AddDefaultComponents(WidgetType p_widget)
+		{
+			GameObject widgetObject = p_widget.gameObject;
+
+			// If the widget has no FollowMouse component attached, add it
+			FollowMouse mouseFollower = widgetObject.GetComponent<FollowMouse>();
+			if(mouseFollower == null)
+			{
+				widgetObject.AddComponent<FollowMouse>();
+			}
+		}
+
+		protected virtual RectTransform GetTooltipParent()
+		{
+			Transform transform = this.transform.parent;
+			if (transform == null)
+			{
+				// this will most likely never be the desired behaviour (but avoids returning null)
+				transform = this.transform;
+			}
+			return (RectTransform)transform;
+		}
+
+
+		public void OnPointerEnter(PointerEventData eventData)
+		{
+			if(this.Delay > 0)
+			{
+				this.Invoke("Show", this.Delay);
+			}
+			else
+			{
+				this.Show();
+			}
+		}
+
+		public void OnPointerExit(PointerEventData eventData)
+		{
+			this.CancelInvoke("Show");
+			this.Hide();
+		}
+	}
+}
