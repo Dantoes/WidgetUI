1222f45493bbafe7a613cef69fca1585f7d5164f
Daniel Reichelt <dr1400@web.de>
2014-12-22 11:31:38 +0100

    Specialized Tooltip version for text

---
+++ WidgetUI/Widgets/Tooltip/Specialized/TextTooltip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace WidgetUI
+{
+	public class TextTooltip
+		: Tooltip<String, TextTooltipWidget>
+	{
+		[SerializeField]
+		[Multiline]
+		protected string m_text;
+
+		[SerializeField]
+		protected TextTooltipWidget m_widgetPrefab;
+
+		protected override string GetContent()
+		{
+			return m_text;
+		}
+
+		protected override IWidgetAllocator<TextTooltipWidget> GetWidgetAllocator()
+		{
+			return new DefaultWidgetAllocator<TextTooltipWidget>(m_widgetPrefab.gameObject);
+		}
+	}
+}
---
+++ WidgetUI/Widgets/Tooltip/Specialized/TextTooltipWidget.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace WidgetUI
+{
+	public class TextTooltipWidget
+		: UIBehaviour
+		, IWidget<String>
+	{
+		public void Enable(string p_text)
+		{
+			Text textComponent = this.gameObject.GetComponentInChildren<Text>();
+			
+			if (textComponent != null)
+			{
+				textComponent.text = p_text;
+			}
+			else
+			{
+				Debug.LogWarning(String.Format("Unable to find Text component on object '{0}' or its children.", this.name));
+			}
+
+		}
+
+		public void Disable()
+		{
+		}
+	}
+}
