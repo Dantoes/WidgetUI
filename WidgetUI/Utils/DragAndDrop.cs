8de890ec14be80c43800ab413b163a73f380782c
Daniel Reichelt <dr1400@web.de>
2015-01-13 17:55:24 +0100

    DragAndDrop behaviour script

---
+++ WidgetUI/Utils/DragAndDrop.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace WidgetUI
+{
+	[RequireComponent(typeof(RectTransform))]
+	public class DragAndDrop
+		: UIBehaviour
+		, IDragHandler
+	{
+		[SerializeField]
+		private RectTransform m_targetTransform;
+
+		public RectTransform TargetTransform
+		{
+			get
+			{
+				if (m_targetTransform == null)
+				{
+					m_targetTransform = this.GetComponent<RectTransform>();
+				}
+
+				return m_targetTransform;
+			}
+			set 
+			{ 
+				m_targetTransform = value;
+			}
+		}
+
+		public void OnDrag(PointerEventData eventData)
+		{
+			this.TargetTransform.anchoredPosition += eventData.delta;
+		}
+	}
+}
